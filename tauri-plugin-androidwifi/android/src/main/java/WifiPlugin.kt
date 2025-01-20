f4d24b9c72a1064649208bd19436025622b5457c
delcin-raj <delcinraj@gmail.com>
2025-01-20 15:25:25 +0530

    Prompting the user for permissions

--- tauri-plugin-androidwifi/android/src/main/java/WifiPlugin.kt
+++ tauri-plugin-androidwifi/android/src/main/java/WifiPlugin.kt
@@ -11,6 +11,10 @@ import app.tauri.plugin.Invoke
 import android.Manifest
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import android.webkit.WebView
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 @InvokeArg
 class Empty {
@@ -27,5 +31,32 @@ class WifiPlugin(private val activity: Activity): Plugin(activity) {
         val json = Json.encodeToString(implementation.getWifiDetails(activity.applicationContext))
         ret.put("wifis", json)
         invoke.resolve(ret)
+    }
+
+    private val REQUEST_CODE_WIFI_PERMISSIONS = 1001
+
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.CHANGE_WIFI_STATE,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    override fun load(webView: WebView) {
+      super.load(webView)
+      // Find any permissions not yet granted.
+      val notGrantedPermissions = requiredPermissions.filter {
+          ContextCompat.checkSelfPermission(activity.applicationContext, it) != PackageManager.PERMISSION_GRANTED
+      }
+
+      // If there are any missing permissions, request them at runtime.
+      if (notGrantedPermissions.isNotEmpty()) {
+          ActivityCompat.requestPermissions(
+              activity,
+              notGrantedPermissions.toTypedArray(),
+              REQUEST_CODE_WIFI_PERMISSIONS
+          )
+      } else {
+          // Permissions already granted. You can safely use the Wi-Fi APIs.
+      }
     }
 }
